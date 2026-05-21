8e644021bdde8e0e2d46aca3a8ff770e51f94cf8
sereq <zyryan.d5@gmail.com>
2026-05-21 17:42:48 +0300

    updated time.js

--- todo_app/static/todo_app/js/time.js
+++ todo_app/static/todo_app/js/time.js
@@ -1,23 +1,31 @@
-const days   = ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'];
-const months = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
 
-let now = new Date()
-let month = now.getMonth()
-let day = now.getDate()
-let weekday = now.getDay()
-let hour = now.getHours()
-let minutes = now.getMinutes()
+function time_now() {
+    let days_num = ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'];
+    let months = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'];
 
-let new_day = document.getElementById("current-date")
-new_day.textContent = days[weekday] + ', ' + day + ' ' + months[month];
-
-let hour_min = document.getElementById("time1")
+    let now = new Date()
+    let month = now.getMonth()
+    let day = now.getDate()
+    let weekday = now.getDay()
+    let hour = now.getHours()
+    let minutes = now.getMinutes()
+    let second = now.getSeconds()
 
 
-if(minutes < 10) {
-    hour_min.innerHTML = hour + ':<span class="muted">0' + minutes + '</span>';
-} else {
-    hour_min.innerHTML = hour + ':<span class="muted">' + minutes + '</span>';
+    let new_day = document.getElementById("current-date")
+    new_day.textContent = days_num[weekday] + ', ' + day + ' ' + months[month];
+
+    let hour_min = document.getElementById("time1")
+
+    if(minutes < 10) {
+        hour_min.innerHTML = hour + ':<span class="muted">0' + minutes + '</span>' ;
+    } else {
+        hour_min.innerHTML = hour + ':<span class="muted">' + minutes + '</span>' ;
+    }
 }
+
+setInterval(time_now, 100);
+
+
 
 
